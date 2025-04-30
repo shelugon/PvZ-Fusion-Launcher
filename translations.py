24476e41a3e4e10e71b7449d93345610f7133b6d
Shel <110683339+shelugon@users.noreply.github.com>
2025-04-29 22:10:16 -0300

    Update

---
+++ translations.py
@@ -0,0 +1,29 @@
+translations = {
+    "pt_BR": {
+        "app_title": "PVZ Fusion Launcher",
+        "settings": "Configurações",
+        "launch_game": "Iniciar Jogo",
+        "manage_mods": "Gerenciar Mods",
+        "mods_folder": "Pasta de Mods",
+        "game_version": "Versão do Jogo: {}",
+        # Adicione todas outras strings aqui
+    },
+    "en_US": {
+        "app_title": "PVZ Fusion Launcher",
+        "settings": "Settings",
+        "launch_game": "Launch Game",
+        "manage_mods": "Manage Mods",
+        "mods_folder": "Mods Folder",
+        "game_version": "Game Version: {}",
+        # Add all other strings here
+    },
+    "es_ES": {
+        "app_title": "PVZ Fusion Launcher",
+        "settings": "Configuración",
+        "launch_game": "Iniciar Juego",
+        "manage_mods": "Administrar Mods",
+        "mods_folder": "Carpeta de Mods",
+        "game_version": "Versión del Juego: {}",
+        # Añadir todas las cadenas aquí
+    }
+}
